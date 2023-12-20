ab79fd2b08947c50da3de2ebd34a3634a3c58362
Maksym <maksim.chepa.worker@gmail.com>
2023-12-20 10:58:29 +0200

    See the list of all users Edit any user - ability to change his role and claim buyerType

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskAuthenticationAuthorization.Models
+{
+    [Authorize(Policy = "AdminOnly")]
+    public class AdminController : Controller
+    {
+        private readonly ShoppingContext _context;
+
+        public AdminController(ShoppingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        [AllowAnonymous]
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Email,TypeOfBuyer,RoleId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Roles = _context.Roles
+                     .Select(role => new SelectListItem
+                     {
+                         Text = role.Name,
+                         Value = role.RoleId.ToString(),
+                         Selected = user.Role != null && role.RoleId == user.RoleId
+                     })
+                     .ToList();
+            ViewBag.BuyerTypes = Enum.GetValues(typeof(BuyerType))
+                         .Cast<BuyerType>()
+                         .Select(v => new SelectListItem
+                         {
+                             Text = v.ToString(),
+                             Value = v.ToString(),
+                             Selected = v == user.TypeOfBuyer
+                         })
+                         .ToList();
+            return View(user);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existingUser = _context.Users.Find(id);
+                    existingUser.RoleId = user.RoleId;
+                    existingUser.Email = user.Email;
+                    existingUser.TypeOfBuyer = user.TypeOfBuyer;
+                    _context.Update(existingUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
